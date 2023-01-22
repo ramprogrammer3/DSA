6517c4286fa3b8199990f689c328a9457aeeb495
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-01-23 04:49:16 +0530

    generic-first

---
+++ OOPs/Generic/Pair.java
@@ -0,0 +1,33 @@
+
+public class Pair{
+    
+    private int first;
+    private int second;
+
+    public Pair(int first, int second){
+        // super();
+        this.first = first;
+        this.second = second;
+    }
+
+    public int getFirst(){
+        return first;
+    }
+
+    public void setFirst(int first){
+        this.first = first;
+    }
+
+
+    public int getSecond(){
+        return second;
+    }
+
+    public void setSecond(int second){
+        this.second = second;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ OOPs/Generic/PairUse.java
@@ -0,0 +1,8 @@
+public class PairUse {
+    
+    public static void main(String[] args) {
+        // System.out.println("hello world");
+        Pair p  = new Pair(10, 20);
+        System.out.println(p.getFirst() + " " + p.getSecond());
+    }
+}
