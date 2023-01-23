137371b24bab7422616e6cf26e23ae362b2893f7
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-01-23 06:02:49 +0530

    generic7

--- OOPs/Generic/Print.java
+++ OOPs/Generic/Print.java
@@ -1,33 +1,34 @@
 public class Print {
 
-    public static<T> void printArray(T arr[]){
+    public static<T extends Vehicle> void printArray(T arr[]){
         for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+            // System.out.print(arr[i] + " ");
+            arr[i].print();
         }
         System.out.println();
     }
     
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // System.out.println("hello world");
 
-        Integer arr[] = new Integer[5];
+        // Integer arr[] = new Integer[5];
 
-        for(int i = 0; i<arr.length; i++){
-            arr[i] = i + 1;
-        }
+        // for(int i = 0; i<arr.length; i++){
+        //     arr[i] = i + 1;
+        // }
 
-        printArray(arr);
+        // printArray(arr);
 
-        String str[] = new String[5];
+        // String str[] = new String[5];
 
-        for(int i = 0; i<str.length; i++){
-            str[i] = "abc";
-        }
-        printArray(str);
+        // for(int i = 0; i<str.length; i++){
+        //     str[i] = "abc";
+        // }
+        // printArray(str);
 
         Vehicle v[] = new Vehicle[5];
         for(int i = 0; i<v.length; i++){
-            v[i] = new Vehicle();
+            v[i] = new Vehicle(10*i, "ram");
         }
         printArray(v);
     }
--- OOPs/Generic/Vehicle.java
+++ OOPs/Generic/Vehicle.java
@@ -2,7 +2,17 @@ public class Vehicle {
 
     int maxSpeed;
     String company;
+
+    public Vehicle(int maxSpeed, String company) {
+        this.maxSpeed = maxSpeed;
+        this.company = company;
+    }
+
+    public void print() {
+        System.out.println(maxSpeed + " " + company);
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
