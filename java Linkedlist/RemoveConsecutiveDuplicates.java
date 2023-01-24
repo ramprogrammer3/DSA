3d1d09fd006e719f145c6fdcc706ec7a1d78f140
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-01-24 07:49:39 +0530

    removeconsecutiveDuplicate

--- java Linkedlist/RemoveConsecutiveDuplicates.java
+++ java Linkedlist/RemoveConsecutiveDuplicates.java
@@ -1,23 +1,27 @@
+import java.util.ArrayList;
+
 public class RemoveConsecutiveDuplicates {
 
-    public static int[] removeConsecutive(int arr[]){
+    public static ArrayList<Integer> removeConsecutiveDuplicate(int arr[]) {
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(arr[0]);
 
-        for(int i = 0; i<arr.length;  i++){
-            if(i + 1 < arr.length){
-                if(arr[i] == arr[i+1]) {
-                    arr[i] = arr[i + 1];
-                    i++;
-                }
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                result.add(arr[i]);
             }
         }
-        return arr;
+        return result;
     }
 
     public static void main(String[] args) {
-        // System.out.println("Hello world");
-        int arr[] = {10, 10, 20,20,20,12,15,23,34};
-        System.out.println(arr.length);
-        removeConsecutive(arr);
-        System.out.println(arr.length);
+        int arr[] = { 10, 10, 20, 20, 20, 30, 40, 15, 23, 0 };
+
+        ArrayList<Integer> result = removeConsecutiveDuplicate(arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i)  + " ");
+        }
+
     }
 }
